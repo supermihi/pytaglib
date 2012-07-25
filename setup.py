7f78e85bea7c4898edc78801b530a9050cc9eff1
Michael Helmling <helmling@mathematik.uni-kl.de>
2012-07-25 23:48:43 +0200

    some more information in setup.py

--- setup.py
+++ setup.py
@@ -13,22 +13,24 @@ from setuptools import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
 
-modules = [Extension("taglib", ["src/taglib.pyx", "src/ctypes.pxd"],
-                     libraries=['tag', "stdc++"],
-                     language="c++")]
+extensions = [Extension('taglib', ['src/taglib.pyx', 'src/ctypes.pxd'],
+                     libraries=['tag', 'stdc++'],
+                     language='c++')]
 
-setup(
-  name = 'pytaglib',
-  description = 'TagLib bindings for python 2.x/3.x, written using cython',
-  version = '0.2.2',
-  author = 'Michael Helmling',
-  author_email = 'helmling@mathematik.uni-kl.de',
-  url = 'http://github.com/supermihi/pytaglib',
   
-  install_requires = ['cython'],
-  cmdclass = {'build_ext': build_ext},
-  ext_modules = modules,
-  scripts = ['src/pyprinttags.py'],
-  package_dir = {'': 'src'},
-  test_suite = "tests",
+setup(
+  name='pytaglib',
+  description='TagLib bindings for python 2.x/3.x, written using cython',
+  versio ='0.2.2',
+  license='GPLv3',
+  author='Michael Helmling',
+  author_email='helmling@mathematik.uni-kl.de',
+  url='http://github.com/supermihi/pytaglib',
+  
+  install_requires=['cython'],
+  cmdclass={'build_ext': build_ext},
+  ext_modules=extensions,
+  scripts=['src/pyprinttags.py'],
+  package_dir={'': 'src'},
+  test_suite='tests',
 )
