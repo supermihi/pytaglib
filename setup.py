e43045775b5de6bd54bab08339f2afbbe5b7c0bb
Michael Helmling <helmling@mathematik.uni-kl.de>
2012-07-25 23:56:00 +0200

    add classifiers

--- setup.py
+++ setup.py
@@ -17,12 +17,21 @@ extensions = [Extension('taglib', ['src/taglib.pyx', 'src/ctypes.pxd'],
                      libraries=['tag', 'stdc++'],
                      language='c++')]
 
-  
+CLASSIFIERS = [
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+    'Natural Language :: English',
+    'Operating System :: OS Independent',
+    'Programming Language :: Cython',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+]
+
 setup(
   name='pytaglib',
   description='TagLib bindings for python 2.x/3.x, written using cython',
   versio ='0.2.2',
-  license='GPLv3',
+  license='GPLv3+',
   author='Michael Helmling',
   author_email='helmling@mathematik.uni-kl.de',
   url='http://github.com/supermihi/pytaglib',
