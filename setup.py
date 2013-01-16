78c24bfc05d9f7b16b5c7b94099a1d444874cad1
Michael Helmling <helmling@mathematik.uni-kl.de>
2013-01-16 22:17:29 +0100

    Fixed some serious packaging issues.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@
 #
 """Setup file for pytaglib. Type <python setup.py install> to install this package."""
  
-import os.path
+import os.path, sys
 from setuptools import setup
 from Cython.Build import cythonize
 
@@ -26,23 +26,28 @@ CLASSIFIERS = [
 
 
 def readme():
-    with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as rm:
-        return rm.read()
+    readmeFile = os.path.join(os.path.dirname(__file__), 'README.rst')
+    if sys.version_info.major >= 3:
+        return open(readmeFile, "rt", encoding='utf-8').read()
+    else:
+        return open(readmeFile, "rt").read()
 
+script_name = 'pyprinttags3' if sys.version_info.major >= 3 else 'pyprinttags'
 
 setup(
     name='pytaglib',
-    description='TagLib bindings for python 2.x/3.x, written using cython',
+    description='Python (2.7+/3.1+) bindings for the TagLib audio metadata library',
     long_description=readme(),
     classifiers=CLASSIFIERS,
-    version='0.3.2',
+    version='0.3.3',
     license='GPLv3+',
     author='Michael Helmling',
     author_email='michaelhelmling@posteo.de',
     url='http://github.com/supermihi/pytaglib',
     install_requires=['cython>=0.16'],
     ext_modules=cythonize("src/taglib.pyx"),
-    entry_points={ 'console_scripts': ['pyprinttags = pyprinttags:script'] },
     package_dir={'': 'src'},
+    py_modules=['pyprinttags'],
+    entry_points={ 'console_scripts': ['{} = pyprinttags:script'.format(script_name)] },
     test_suite='tests'
 )
